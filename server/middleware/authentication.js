b20efe3d854f8f04246d58ed234570eddb2fed21
Rajneesh Yadav <rajnishyadav11072002@gmail.com>
2024-07-24 09:38:24 +0530

    creating authentication middleware

---
+++ server/middleware/authentication.js
@@ -0,0 +1,30 @@
+const { verifyAccessToken } = require("../lib/helper/common");
+const { http } = require("../lib/helper/const");
+
+exports.auth = (req, res, next) => {
+  const token = req.headers.authorization.trim().split(" ")[1];
+
+  try {
+    if (!token) {
+      res
+        .status(http.INTERNAL_SERVER_ERROR)
+        .send({ msg: "error", error: "token configuration is missing" });
+    }
+
+    const decoded = verifyAccessToken(token);
+
+    if (!decoded) {
+      return res.status(http.UNAUTHORIZED).send({
+        msg: "error",
+        error: "Token is invalid, please login again!",
+      });
+    }
+    req.body = { ...req.body, ...decoded };
+    next();
+  } catch (error) {
+    return res.status(http.BAD_REQUEST).send({
+      msg: "error",
+      error: "authentication fail!",
+    });
+  }
+};
